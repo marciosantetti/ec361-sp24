36889155579f8493811faa808cc7a48e9b4c3775
Marcio Santetti <santetti@gmail.com>
2024-02-12 09:05:41 -0500

    ps1

--- lectures/003-graphics/acf.R
+++ lectures/003-graphics/acf.R
@@ -13,19 +13,51 @@ beer <- aus_production |>
 
 beer |> 
   as_tibble() |> 
+  ggplot(aes(x = Quarter, y = Beer)) +
+  geom_line() +
+  geom_point()
+
+beer |> 
+  as_tibble() |> 
   mutate(period = seq(1, nrow(beer), 1)) |> 
+  mutate(lbeer = lag(Beer, 1),
+         lbeer2 = lag(Beer, 2),
+         lbeer3 = lag(Beer, 3),
+         lbeer4 = lag(Beer, 4),
+         lbeer5 = lag(Beer, 5)) |> 
   summarize(mean_beer = sum(Beer) / nrow(beer),
             variance = (1 / nrow(beer)) * sum((Beer - mean_beer)^2),
-            autocovariance = (1 / nrow(beer)) * sum((Beer - mean_beer) * (l_beer - mean_beer), na.rm = TRUE),
-            acf = autocovariance / variance)
+            autocovariance1 = (1 / nrow(beer)) * sum((Beer - mean_beer) * (lbeer - mean_beer), na.rm = TRUE),
+            autocovariance2 = (1 / nrow(beer)) * sum((Beer - mean_beer) * (lbeer2 - mean_beer), na.rm = TRUE),
+            autocovariance3 = (1 / nrow(beer)) * sum((Beer - mean_beer) * (lbeer3 - mean_beer), na.rm = TRUE),
+            autocovariance4 = (1 / nrow(beer)) * sum((Beer - mean_beer) * (lbeer4 - mean_beer), na.rm = TRUE),
+            autocovariance5 = (1 / nrow(beer)) * sum((Beer - mean_beer) * (lbeer5 - mean_beer), na.rm = TRUE),
+            acf1 = autocovariance1 / variance,
+            acf2 = autocovariance2 / variance,
+            acf3 = autocovariance3 / variance,
+            acf4 = autocovariance4 / variance,
+            acf5 = autocovariance5 / variance)
 
 
 beer |> 
-  ACF(lag_max = 9)
+  ACF(lag_max = 5)
 
-beer |> 
-  gg_lag(geom = "point", lags = 1:9)
 
+USgas::us_total |> 
+  as_tsibble(index = year,
+             key = state) |> 
+  filter(state == "New York") |> 
+  ACF() |> 
+  autoplot()
+
+
+USgas::us_total |> 
+  as_tsibble(index = year,
+             key = state) |> 
+  filter(state == "New York") |>
+  ggplot(aes(y = y, x = year)) +
+  geom_point() +
+  geom_line()
 
 
 
@@ -84,4 +116,16 @@ elec |>
   gg_lag(geom = "point")
 
 beer |> 
+  autoplot()
+
+
+
+#################
+
+gafa_stock |>
+  filter(Symbol == "GOOG", year(Date) >= 2018) |>
+  mutate(trading_day = row_number()) |>
+  update_tsibble(index = trading_day, regular = TRUE) |>
+  mutate(diff = difference(Close)) |>
+  #ACF(diff) |>
   autoplot()
